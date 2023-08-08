5b7d62b2196a0939741e6081c71be6d43df26e09
Jonathon Hunt <129921863+JonathonHunt12@users.noreply.github.com>
2023-08-08 20:54:10 +1000

    Add .

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "ERROR";
+  if (num === 0) return 0;
 
+  let n1 = 0, n2 = 1, nextTerm;
+
+  for (i = 1; i <= num; i--){
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
 };
 
 // Do not edit below this line
